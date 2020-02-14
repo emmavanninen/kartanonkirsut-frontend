90dfb5c2ea94240c771196db6ff66db29297f85b
emmavanninen <emmavanninen@gmail.com>
2020-02-14 16:31:53 -0500

    sections on home and email form base + google maps

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import './Contact.css'
+
+class Contact extends Component {
+  state = {
+    body: ""
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+    console.log("email sent")
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <form action="">
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full Name"
+          onChange={this.handleChange}
+          value={this.state.username}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={this.handleChange}
+          value={this.state.email}
+        />
+        <input
+          type="text"
+          name="subject"
+          placeholder="Subject"
+          onChange={this.handleChange}
+          value={this.state.username}
+        />
+        <textarea
+          className="writeMsgInput"
+          name="body"
+          value={this.state.body}
+          onChange={this.handleChange}
+          placeholder="Write a message..."
+        />
+        <input
+          className="sendEmailButton"
+          type="submit"
+          value="Send email"
+          onClick={this.handleSubmit}
+        />
+      </form>
+    )
+  }
+}
+
+export default Contact
