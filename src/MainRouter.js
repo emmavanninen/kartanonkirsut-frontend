369d571dff5dc8391a5241022fa6adcb47e719e1
emmavanninen <emmavanninen@gmail.com>
2020-02-14 15:09:52 -0500

    header style base

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -4,24 +4,23 @@ import { Route, Switch } from "react-router-dom"
 import Nav from "./components/Nav/Nav"
 import Footer from "./components/Footer/Footer"
 
-const Home = React.lazy(() => import("./components/Home/Home"));
-const About = React.lazy(() => import("./components/About/About"));
-const Rates = React.lazy(() => import("./components/Rates/Rates"));
-const Booking = React.lazy(() => import("./components/Booking/Booking"));
+const Home = React.lazy(() => import("./components/Home/Home"))
+const About = React.lazy(() => import("./components/About/About"))
+const Rates = React.lazy(() => import("./components/Rates/Rates"))
+const Booking = React.lazy(() => import("./components/Booking/Booking"))
 
 export default class MainRouter extends Component {
-
   render() {
     return (
       <div>
-        <Nav/>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/about-us" component={About}></Route>
           <Route exact path="/rates" component={Rates}></Route>
           <Route exact path="/booking" component={Booking}></Route>
         </Switch>
-        <Footer/>
+        <Footer />
       </div>
     )
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                HEADER
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="header">
+        <div className="titlediv">
+          {" "}
+          <h5 className="title">Kartanon Kirsut</h5>
+          <h6 className="subtitle">Kartanon Kirsut</h6>
+        </div>
+      </div>
+    )
+  }
 }
